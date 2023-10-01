3df6ade102d1ef4053ae1bd37923004ed03a283c
navnath-auti <navnathauti0510gmail.com>
2023-10-01 21:23:43 +0530

    connection with atlas mongodb

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const productRoutes = require("./routes/product");
+const dotenv = require("dotenv");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -10,16 +11,29 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect("mongodb://127.0.0.1:27017/AdminPanel", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+dotenv.config({ path: "./config.env" });
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
 
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "MongoDB connection error:"));
-db.once("open", () => {
-  console.log("Connected to MongoDB");
-});
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB connection successful!"));
+
+// mongoose.connect("mongodb://127.0.0.1:27017/AdminPanel", {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+
+// const db = mongoose.connection;
+// db.on("error", console.error.bind(console, "MongoDB connection error:"));
+// db.once("open", () => {
+//   console.log("Connected to MongoDB");
+// });
 
 app.use("/api/products", productRoutes);
 
